d2c26f3036631b72d449616ce0d4f952644d526e
martinfuchs <martinfuchs@c1a74d26-56c6-cf42-9917-11a49294286f>
2009-01-25 13:07:05 +0000

    gedmurphy: Set the start button as the owner of the root start menu. (with small editorial improvements)
    git-svn-id: http://shell.franken.de/svn/sky/ros-explorer@1267 c1a74d26-56c6-cf42-9917-11a49294286f

--- trunk/explorer/taskbar/desktopbar.cpp
+++ trunk/explorer/taskbar/desktopbar.cpp
@@ -168,6 +168,7 @@ LRESULT DesktopBar::Init(LPCREATESTRUCT pcs)
 
 	 // prepare Startmenu, but hide it for now
 	_startMenuRoot = GET_WINDOW(StartMenuRoot, StartMenuRoot::Create(_hwnd, STARTMENUROOT_ICON_SIZE));
+	_startMenuRoot->_hwndStartButton = _hwndStartButton;
 
 	return 0;
 }
